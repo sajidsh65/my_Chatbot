fdb01717e3f0c496ecd18a7c6244cff4f6d41cb2
sajidsh65 <158176359+sajidsh65@users.noreply.github.com>
2025-04-11 17:01:43 +0500

    Update urls.py

--- myChatbot/urls.py
+++ myChatbot/urls.py
@@ -6,13 +6,13 @@ from .views import (
     register_user, login_user,get_all_chat_histories, logout_user, chatbot_response, create_session)
 
 urlpatterns = [
-    path('register/', register_user, name='register'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
-    path('token/', obtain_auth_token, name='api_token_auth'),
-    path('chat/', chatbot_response, name='chatbot_response'),
-    path("create-session/", create_session, name="create_session"),
-    path("all-chat-histories/", get_all_chat_histories, name="all-chat-histories"),
+    path('api/register/', register_user, name='register'),
+    path('api/login/', login_user, name='login'),
+    path('api/logout/', logout_user, name='logout'),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
+    path('api/chat/', chatbot_response, name='chatbot_response'),
+    path("api/create-session/", create_session, name="create_session"),
+    path("api/all-chat-histories/", get_all_chat_histories, name="all-chat-histories"),
 
 ]
 
