e154f74009ca6745b6d34c599d6b48d27599d54f
sajidsh65 <158176359+sajidsh65@users.noreply.github.com>
2025-04-11 20:03:52 +0500

    Update settings.py

--- ai_chatbot/settings.py
+++ ai_chatbot/settings.py
@@ -104,16 +104,16 @@ WSGI_APPLICATION = 'ai_chatbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mychatdb',  # Tumhara database ka naam
-        'USER': 'postgres',  # Default PostgreSQL user
-        'PASSWORD': 'sajid6576',  # Jo installation ke waqt set ki thi
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mychatdb',  # Tumhara database ka naam
+#         'USER': 'postgres',  # Default PostgreSQL user
+#         'PASSWORD': 'sajid6576',  # Jo installation ke waqt set ki thi
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
