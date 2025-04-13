dd2887590b19bbf3faf4537bd3f01dfa216ca981
sajidsh65 <158176359+sajidsh65@users.noreply.github.com>
2025-04-13 10:01:01 +0500

    Update base.py

--- ai_chatbot/settings/base.py
+++ ai_chatbot/settings/base.py
@@ -11,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-secret-key")
 DEBUG = False
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "chatbot-bysajid-3685.up.railway.app",
-]
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = [
+#     "localhost",
+#     "127.0.0.1",
+#     "chatbot-bysajid-3685.up.railway.app",
+# ]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
