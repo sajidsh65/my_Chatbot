772a848657656ac1b16ae1ab4e56a247d38ac277
sajidsh65 <158176359+sajidsh65@users.noreply.github.com>
2025-04-13 09:41:21 +0500

    Update base.py

--- ai_chatbot/settings/base.py
+++ ai_chatbot/settings/base.py
@@ -95,7 +95,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://chatbot-bysajid-3685.up.railway.app",  # optional
 ]
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOW_HEADERS = [
     "content-type",
     "authorization",
