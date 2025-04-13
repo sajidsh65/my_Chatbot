3dfba13953c5e8019fe0682ffd56476be09323a2
sajidsh65 <158176359+sajidsh65@users.noreply.github.com>
2025-04-13 11:16:48 +0500

    Update base.py

--- ai_chatbot/settings/base.py
+++ ai_chatbot/settings/base.py
@@ -96,10 +96,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 # Allow local frontend
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://chatbot-bysajid.vercel.app/",
     "https://chatbot-bysajid-3685.up.railway.app",  # optional
 ]
 
